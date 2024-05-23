8a298ea3542054ae25adcc65a7ed8b2555769607
Gonzalo Palomo <pgonzalojose@gmail.com>
2024-05-23 14:51:32 -0300

    using the localStorage

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
 const form = document.getElementById('form');
+let word = document.getElementById('word');
 
 //Here get the input value and that way can get the value using anothe event
 const takeValue = () =>{
-    const word = document.getElementById('word').value;
+    word = document.getElementById('word').value;
     const li = document.createElement('li');
     li.innerHTML = `<li>${word}</li>`;
     const fieldWork = document.getElementById('fieldWord');
@@ -13,11 +14,21 @@ const takeValue = () =>{
 form.addEventListener('submit',(e) => {
     e.preventDefault();
     takeValue();
+    inValue();
 });
 
 form.addEventListener('keydown', (e) => {
     if(e.key === 'Enter'){
         e.preventDefault();
         takeValue();
+        inValue();
     }
 });
+
+
+const inValue = () =>{
+    word = document.getElementById('word').value;
+    console.log("este es la palabra desde el metodo invalue " + word)
+    localStorage.setItem('word', word);
+    localStorage.getItem('word');
+}
