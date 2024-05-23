4e9f3dc1a0e4d15671e331949734934b066307b4
Gonzalo Palomo <pgonzalojose@gmail.com>
2024-05-22 22:20:09 -0300

    create a separate function to be able to use in other event

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 
 const form = document.getElementById('form');
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Capturo el valor del input
+//Here get the input value and that way can get the value using anothe event
+const takeValue = () =>{
     const word = document.getElementById('word').value;
-    const fieldWork = document.getElementById('fieldWord');
     const li = document.createElement('li');
     li.innerHTML = `<li>${word}</li>`;
+    const fieldWork = document.getElementById('fieldWord');
     fieldWork.appendChild(li);
+}
 
-    console.log(word);
+form.addEventListener('submit',(e) => {
+    e.preventDefault();
+    takeValue();
+});
+
+form.addEventListener('keydown', (e) => {
+    if(e.key === 'Enter'){
+        e.preventDefault();
+        takeValue();
+    }
 });
